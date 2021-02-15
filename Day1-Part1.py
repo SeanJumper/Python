f38520ae29e97cd96406e2be70b17f7cad7e5b66
SeanPRyan <sean23397@gmail.com>
2021-02-15 21:04:25 +0000

    Day 1

--- Day1-Part1.py
+++ Day1-Part1.py
@@ -1,3 +1,12 @@
 a = 8
-b = 4
-print('Assign Values:\t\t', 'a =', a, '\tb =', b)
+b = 2
+print('Assigned Values:\t\t', 'a =', a, '\tb =', b)
+print('Addition:\t', a, '+', b, '=', a + b )
+print('Subtraction:\t', a, '-', b, '=', a - b)
+print('Multiplation:\t', a, 'x', b, '=', a - b)
+print('Division:\t', a, '/', b, '=', a/b)
+print('Floor Devision:\t', a, '%', b, '=', a//b)
+print('Modulo:\t', a, '~', b, '=', a % b)
+
+print('Exponent:\t', a, '^2 =', a**b, sep ='')
+
