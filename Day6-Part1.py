8e6caf4c42393a15818ee5779ed1f31ae07b9078
SeanPRyan <sean23397@gmail.com>
2021-04-29 23:01:36 +0100

    📒

---
+++ Day6-Part1.py
@@ -0,0 +1,14 @@
+#Multi-dimensional Lists
+quarter = [['Jan', 'Feb', 'Mar'], ['1/1/21', '1/2/21', '1/3/21']]
+print("Month:", quarter[0][0] )
+print("Date:", quarter[1][0] )
+
+print("\nMonth:", quarter[0][1] )
+print("Date:", quarter[1][1]  )
+
+print("\nMonth:", quarter[0][2] )
+print("Date:", quarter[1][2] )
+
+
+
+
