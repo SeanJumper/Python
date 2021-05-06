5636d05dccec56bf74cef30ef9d7aff76ad94984
SeanPRyan <sean23397@gmail.com>
2021-05-06 20:39:42 +0100

    📑

---
+++ Day7-Part1.py
@@ -0,0 +1,26 @@
+#Reading and Writing Files 
+#
+#
+#Example taken from: Python Auth Mike McGrath 
+#
+
+## Command line entries ##
+#
+#
+## poem += 'I never saw a man who looked \n' 
+## poem += 'With Such a wistful eye \n' 
+## poem += 'Upon that little tint of blue \n'
+## poem += 'Which prisoners call the sky \n' 
+# 
+# 
+##file = open('poem.txt') 
+#
+##file.write(poem)
+##file.close
+#
+##file = open('poem.txt', 'r')
+#
+#
+## for line in file 
+##      print(line, end = '')
+## file.close
