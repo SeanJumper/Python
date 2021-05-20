4f7cfece1f13de14fda334e90b42845f0d151762
SeanPRyan <sean23397@gmail.com>
2021-05-20 20:45:45 +0100

    💹💲

---
+++ Day11.py
@@ -0,0 +1,20 @@
+#Example modified from www.holypython.com
+def percentage(x,y):
+    percentage_as_a_percent = profit / Salesint
+    percentage_as_a_percent_numero = percentage_as_a_percent *100
+    print(percentage_as_a_percent_numero)
+
+Sales = input("Please enter the sales:\n")
+Salesint = int(Sales)
+Cost = input("How much did you buy the products for?\n")
+Costint = int(Cost)
+profit =int(Salesint - Costint)
+
+percentage(profit, Costint)
+
+
+
+
+
+
+
