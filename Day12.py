e23c4a485d517c72359502f9586350cad0db7b38
SeanPRyan <sean23397@gmail.com>
2021-05-21 19:23:14 +0100

    🍕

---
+++ Day12.py
@@ -0,0 +1,9 @@
+#Slicing - Simple Slice
+
+slice = input("Please enter your name: \n")
+#Print Every Letter from start to finish
+for i in str(slice):
+    print(i)
+
+#Print all from second letter 
+print(slice[1:])
